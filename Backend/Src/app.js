a7b10eacf510bf70d7493a260167683126fe6785
Nanda Bendkoli <bendkolinanda12@gmail.com>
2026-05-25 21:50:17 +0530

    added frontened url

--- Backend/Src/app.js
+++ Backend/Src/app.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        // "vercel"
+        "https://resume-forge-ai-ndwm.vercel.app"
     ],
 
     credentials: true
